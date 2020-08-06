efb8dcc3909150dd451c68b565282495273a799d
Bohdana Yurchenko <bogdanaiurchienko@gmail.com>
2020-08-06 23:38:37 +0300

    finished the guessgame

---
+++ guessinggame.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash	
+
+function getmessage {
+	local guess=$1
+	local real=$2
+
+	if [[ $guess -gt $real ]]
+		then echo "too high"
+		else echo "too low"
+	fi
+}
+
+function game {
+	local guessed=0
+	local numberoffiles=$( ls -l | wc -l )
+
+	while [[ $guessed -eq 0 ]]
+	do 
+		echo "Enter the number of the files in the directory"
+		read number 
+		
+		if [[ $number -eq $numberoffiles ]]	
+		then 
+			echo "The number of files is right! Congratulations!"
+			let guessed=1
+		elif [[ $number =~ [^0-9] ]]
+			then echo "please, enter a natural number N (N >= 0)" 
+		else 
+			message=$( getmessage $number $numberoffiles )
+			echo "A number is $message"
+		fi
+done 
+}
+
+game
+
+
